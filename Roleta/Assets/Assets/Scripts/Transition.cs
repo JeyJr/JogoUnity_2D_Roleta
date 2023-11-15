35d7972014e324b99a5ec70ef2659a39af1d370e
José Junior <jeyjr.dg@gmail.com>
2023-11-15 15:38:29 -0300

    Add scripts de controle do GameState

---
+++ Roleta/Assets/Assets/Scripts/Transition.cs
@@ -0,0 +1,49 @@
+﻿using Assets.Assets.Scripts.Enums;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Assets.Scripts
+{
+    public class Transition : MonoBehaviour
+    {
+        Animator anim;
+        public float AnimDelay { get; private set; } = 0f;
+
+        private void Awake()
+        {
+            anim = GetComponent<Animator>();
+            SetAnimationDuration("TransitionFade");
+            this.gameObject.SetActive(false);
+        }
+
+        private void SetAnimationDuration(string animationName)
+        {
+            AnimatorStateInfo animStateInfo = anim.GetCurrentAnimatorStateInfo(0);
+            int animationHash = Animator.StringToHash(animationName);
+
+            if (animStateInfo.shortNameHash == animationHash)
+            {
+                AnimDelay = animStateInfo.length;
+            }
+        }
+
+        public void StartTransition()
+        {
+            anim.Play("TransitionFade");
+            this.gameObject.SetActive(true);
+
+            StartCoroutine(EndTransition());
+        }
+
+        private IEnumerator EndTransition()
+        {
+            yield return new WaitForSeconds(AnimDelay);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Roleta/Assets/Assets/Scripts/UIController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Assets.Scripts
+{
+    public class UIController : MonoBehaviour
+    {
+        public List<GameObject> panels = new List<GameObject>();
+        public Transition transition;
+
+        private void Awake()
+        {
+            
+        }
+
+        public void BtnPlayGame()
+        {
+            transition.StartTransition();
+            StartCoroutine(EndTransition("PanelRoulette"));
+            StartCoroutine(EncerrarRoullete());
+        }
+
+        public IEnumerator EncerrarRoullete()
+        {
+            yield return new WaitForSeconds(1);
+            transition.StartTransition();
+            StartCoroutine(EndTransition("PanelEndGame"));
+        }
+
+        public void BtnEndGame()
+        {
+            transition.StartTransition();
+            StartCoroutine(EndTransition("PanelMainMenu"));
+        }
+
+        private IEnumerator EndTransition(string panelNameToActive)
+        {
+            yield return new WaitForSeconds(transition.AnimDelay / 2);
+
+            panels.ForEach(p =>
+            {
+                p.SetActive(false);
+
+                if (p.name == panelNameToActive)
+                    p.SetActive(true);
+            });
+        }
+    }
+}
